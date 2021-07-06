11a4cbe08cece9c31400d1d47b307473d1051a89
Bruno Russi Lautenschlager <bruno.lautenschlager@ifood.com.br>
2021-07-06 20:11:34 -0300

    feat: remove infra

--- main.tf
+++ main.tf
@@ -23,6 +23,7 @@ terraform {
 resource "aws_s3_bucket" "bucket_site" {
     bucket = "bruno-atlantis-rename"
     acl    = "private"
+
 }
 
 resource "aws_sqs_queue" "terraform_queue" {
@@ -31,6 +32,7 @@ resource "aws_sqs_queue" "terraform_queue" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
+  
   
   tags = {
     Environment = "production"
