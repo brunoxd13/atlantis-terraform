5ccd64c7076bec1fb48a354efe6ecdb72e907ddd
Bruno Russi Lautenschlager <bruno.lautenschlager@ifood.com.br>
2021-07-06 19:45:48 -0300

    feat: add sqs queue for presentation example

--- main.tf
+++ main.tf
@@ -24,3 +24,15 @@ resource "aws_s3_bucket" "bucket_site" {
     bucket = "bruno-atlantis-rename"
     acl    = "private"
 }
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "bruno-atlantis-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  
+  tags = {
+    Environment = "production"
+  }
+}
