4bf47d0941789c8c53983e29ba9ef92177bf242b
Bruno Russi Lautenschlager <bruno.lautenschlager@ifood.com.br>
2021-07-19 08:36:13 -0300

    feat: add s3 bucket

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ provider "aws" {
 }
 
 ############################################################
-# Configuration for the provider used by terraform
-# i.e: AWS provider - resources will be created on AWS
+# Configuration for the terraform state file
 ############################################################
 
 terraform {
@@ -21,20 +20,6 @@ terraform {
 }
 
 resource "aws_s3_bucket" "bucket_site" {
-    bucket = "bruno-atlantis-rename"
+    bucket = "bruno-atlantis"
     acl    = "private"
-
-}
-
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "devparana-atlantis-example-queue"
-  delay_seconds             = 40
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
-  
-  
-  tags = {
-    Environment = "production"
-  }
 }
